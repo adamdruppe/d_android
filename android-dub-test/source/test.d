b23f004421bedb3f1dbd775b0ef25f8b55eb1d44
Adam D. Ruppe <destructionator@gmail.com>
2019-12-31 23:08:55 -0500

    show new options

--- android-dub-test/source/test.d
+++ android-dub-test/source/test.d
@@ -2,23 +2,28 @@ module test;
 
 import arsd.jni;
 
-//import android.java.android.widget.TextView;
-
 // it is possible to import Java methods from Android APIs
-class TextView : IJavaObject { // JavaClass!("android.widget", TextView) {
+import android.java.android.widget.TextView;
+
+// it is important to import the module for any Java class you actually
+// call methods on btw, even if the compiler doesn't force you to. If
+// you do not explicitly import the module, the implementations will not
+// be linked in.
+
+/+
+// or you can declare just what you specifically need (also gives faster builds!)
+// this demos the long-form thing
+final class TextView : IJavaObject { // JavaClass!("android.widget", TextView) {
 	@Import void setTextColor(int);
 	mixin IJavaObjectImplementation!(false);
         mixin JavaPackageId!("android.widget", "TextView");
-	mixin ImportExportImpl!TextView;
 }
+mixin ImportExportImpl!TextView;
++/
 
-// as well as export your own native methods for JNI to call:
-class MainActivity : IJavaObject { // JavaClass!("com.example.basicactivity", MainActivity) {
-	/*
-	@Export wstring stringFromJNI() {
-		return "Hello, this is D!"w;
-	}
-	*/
+// as well as export your own native methods for JNI to call pretty easily:
+// this shows the short-form implementation
+final class MainActivity : JavaClass!("com.example.basicactivity", MainActivity) {
 
 	// the object will be passed in from Java to change the color
 	// via the Java method. Triggered on click in the sample program.
@@ -26,9 +31,9 @@ class MainActivity : IJavaObject { // JavaClass!("com.example.basicactivity", Ma
 		tv.setTextColor(0xffff00ff);
 	}
 
-	mixin IJavaObjectImplementation!(false);
-	mixin JavaPackageId!("com.example.basicactivity", "MainActivity");
+	// can also return values from D to Java
+	@Export wstring saySomething() {
+		return "Hello from D!"w;
+	}
 }
-
-mixin ImportExportImpl!MainActivity;
 
